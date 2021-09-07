3d1fe8ce34e117f7816707d0aba933a33cd8f65a
evgenijh <evgenijh7@gmail.com>
2021-09-07 18:36:17 +0300

    hw10

--- src/ExampleException.java
+++ src/ExampleException.java
@@ -1,8 +1,10 @@
 public class ExampleException {
-  public static int rectangleArea(int a, int b) {
-    // TODO put your code here
-    return 0;
-  }
+    public static int rectangleArea(int a, int b) {
+        int area;
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("input value is below zero!");
+        } else
+            area = a * b;
+        return area;
+    }
 }
-
-
